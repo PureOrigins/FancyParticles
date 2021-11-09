842dd4b1117a1125f1af30118425e567bfb09594
Raymark97 <marcoliga97@gmai.com>
2021-11-09 14:43:52 +0100

    autistic indentation fix

--- src/main/java/mixin/AdvancementRewardsAccessor.java
+++ src/main/java/mixin/AdvancementRewardsAccessor.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AdvancementRewards.class)
-        public interface AdvancementRewardsAccessor {
+public interface AdvancementRewardsAccessor {
     @Accessor
     int getExperience();
 
